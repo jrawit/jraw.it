cc00da3a7d02019ce81bb19bb508e77217f374be
AidasCep <152422494+AidasCep@users.noreply.github.com>
2025-05-07 02:06:47 +0300

    Updated Circle Component Using Skia's Oval

--- components/tools/Circle.tsx
+++ components/tools/Circle.tsx
@@ -1,13 +1,13 @@
 import { CanvasElements } from '@/constants/CanvasElement';
 import {
-  Paint,
+  Group,
+  Oval,
   PaintStyle,
   SkCanvas,
   Skia,
   SkPaint,
-  Path as SkPath,
 } from '@shopify/react-native-skia';
-import React, { useMemo } from 'react';
+import React from 'react';
 
 interface CircleProps {
   circleData: CanvasElements.Circle;
@@ -17,25 +17,29 @@ export const Circle: React.FC<CircleProps> = React.memo(({ circleData }) => {
   const { center, radiusX, radiusY, strokeColor, strokeWidth, fillColor } =
     circleData;
 
-  // Create an elliptical path
-  const path = useMemo(() => {
-    const path = Skia.Path.Make();
-    path.addOval(
-      Skia.XYWHRect(
-        center.x - radiusX,
-        center.y - radiusY,
-        radiusX * 2,
-        radiusY * 2
-      )
-    );
-    return path;
-  }, [center.x, center.y, radiusX, radiusY]);
+  const x = center.x - radiusX;
+  const y = center.y - radiusY;
+  const width = radiusX * 2;
+  const height = radiusY * 2;
 
   return (
-    <SkPath path={path} style="stroke">
-      {fillColor && <Paint color={fillColor} />}
-      <Paint color={strokeColor} style="stroke" strokeWidth={strokeWidth} />
-    </SkPath>
+    <Group>
+      {/* Draw fill first if specified */}
+      {fillColor && (
+        <Oval x={x} y={y} width={width} height={height} color={fillColor} />
+      )}
+
+      {/* Draw stroke */}
+      <Oval
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        color={strokeColor}
+        style="stroke"
+        strokeWidth={strokeWidth}
+      />
+    </Group>
   );
 });
 
@@ -47,27 +51,19 @@ export const renderCircle = (
   const { center, radiusX, radiusY, strokeColor, strokeWidth, fillColor } =
     circleData;
 
-  // Create an elliptical path
-  const path = Skia.Path.Make();
-  path.addOval(
-    Skia.XYWHRect(
-      center.x - radiusX,
-      center.y - radiusY,
-      radiusX * 2,
-      radiusY * 2
-    )
-  );
+  const x = center.x - radiusX;
+  const y = center.y - radiusY;
+  const width = radiusX * 2;
+  const height = radiusY * 2;
 
-  // Draw fill first
   if (fillColor) {
     paint.setStyle(PaintStyle.Fill);
     paint.setColor(Skia.Color(fillColor));
-    canvas.drawPath(path, paint);
+    canvas.drawOval(Skia.XYWHRect(x, y, width, height), paint);
   }
 
-  // Draw stroke
   paint.setStyle(PaintStyle.Stroke);
   paint.setStrokeWidth(strokeWidth);
   paint.setColor(Skia.Color(strokeColor));
-  canvas.drawPath(path, paint);
+  canvas.drawOval(Skia.XYWHRect(x, y, width, height), paint);
 };
